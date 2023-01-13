2d1a699fa99e42088eff6d01759b2e6937b50418
geuntae.kim <kok617@naver.com>
2023-01-13 22:21:08 +0900

    Add: null safety scope check

--- nullSafety.dart
+++ nullSafety.dart
@@ -11,4 +11,12 @@ void main() {
 
   // nullable
   name2 = null;
+
+  // null safety 때문에 length 불가, null 이 될수 있으므로 null 을 참조하지않도록 오류가 남. 컴파일 전에 코드에서 수정하도록함.
+  // print(name2.length);
+
+  if (name2 != null) {
+    // null 체크를 했으므로, length 사용 가능.
+    print(name2.length);
+  }
 }
