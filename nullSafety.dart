82233f1ec6a4f80fb59b13b2d9a88a876ba88ca5
geuntae.kim <kok617@naver.com>
2023-01-13 22:14:37 +0900

    Add: nullalbe 변수

--- nullSafety.dart
+++ nullSafety.dart
@@ -3,4 +3,12 @@ bool isEmpty(String str) => str.length == 0;
 
 void main() {
   // isEmpty(null);
+  String name = 'kim';
+  String? name2 = 'kim';
+
+  // null 선언 불가
+  // name = null;
+
+  // nullable
+  name2 = null;
 }
