2526dcf7ddc5b4fe8396cc9c2e279e8097a87017
geuntae.kim <kok617@naver.com>
2023-01-13 21:02:49 +0900

    var dynamic type check

--- main.dart
+++ main.dart
@@ -1,3 +1,16 @@
 void main() {
-  print('hello world!');
+  var name1 = '';
+  String name2 = '';
+
+  print('name1 is ${name1.runtimeType}');
+  print('name2 is ${name2.runtimeType}');
+
+  var name3;
+  name3 = '';
+  name3 = false;
+  dynamic name4 = '';
+  name4 = false;
+
+  print('name3 is ${name3.runtimeType}');
+  print('name3 is ${name4.runtimeType}');
 }
