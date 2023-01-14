078b482d78fc8038359ffbd487ff297d409ad70b
geuntae.kim <kok617@naver.com>
2023-01-14 14:00:11 +0900

    Add: comment

--- stringInterpolation.dart
+++ stringInterpolation.dart
@@ -1,7 +1,8 @@
 void main(List<String> args) {
   var name = 'kim';
   var age = 10;
-  var greeting = 'hello, my name is $name and I am ${age = 2}';
+  // $: 변수만, ${} 처리결과를 넣을때
+  var greeting = 'hello, my name is $name and I am ${age + 2}';
 
   print(greeting);
 }
