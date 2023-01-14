7275fd7cfe1a21030e8add62e931539d564fe3e3
geuntae.kim <kok617@naver.com>
2023-01-14 13:45:57 +0900

    Add: list내에 if 사용 예시

--- list.dart
+++ list.dart
@@ -1,8 +1,14 @@
 void main(List<String> args) {
+  var giveMeFive = true;
   var numbers = [
     1,
     2,
     3,
     4,
+    if (giveMeFive) 5, // list내에 if 추가가능.
   ];
+
+  // if (giveMeFive) {
+  //   numbers.add(5);
+  // }
 }
