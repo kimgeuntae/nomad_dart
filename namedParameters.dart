ad3aefc53318094011a1d540320b750c1656321b
geuntae.kim <kok617@naver.com>
2023-01-25 22:22:31 +0900

    Add: named func

--- namedParameters.dart
+++ namedParameters.dart
@@ -2,6 +2,26 @@ String sayHelloBasic(String name, int age, String country) {
   return 'Hello $name, you are $age, from $country.';
 }
 
+// null safety
+// String sayHelloNamed({String name, int age, String country}) {
+String sayHelloNamed({
+  String name = 'anon',
+  int age = 99,
+  String country = 'Japan',
+}) {
+  return 'Hello $name, you are $age, from $country.';
+}
+
 void main() {
   print(sayHelloBasic('kim', 20, 'USA'));
+  print(sayHelloNamed(
+    age: 12,
+    country: 'cuba',
+    name: 'mike',
+  ));
+  print(sayHelloNamed(
+    // age: 12,
+    country: 'cuba',
+    // name: 'mike',
+  )); // Hello anon, you are 99, from cuba.
 }
