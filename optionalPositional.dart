241c6c6b1a4680fcd4124a07279faba896c554ee
geuntae.kim <kok617@naver.com>
2023-01-26 23:13:06 +0900

    Ref: optional positional

--- optionalPositional.dart
+++ optionalPositional.dart
@@ -1,7 +1,10 @@
-String sayHello(String name, int age, String country) =>
+// String sayHello(String name, int age, String country) =>
+//     'Hello $name, you are $age years old from $country';
+
+String sayHello(String name, int age, [String? country = 'cuba']) =>
     'Hello $name, you are $age years old from $country';
 
 void main() {
-  var result = sayHello('kim', 11, 'korea');
+  var result = sayHello('kim', 11);
   print(result);
 }
