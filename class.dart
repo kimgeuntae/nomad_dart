769b5d39aa1dfa990635ba4048e56905e7154a5f
geuntae.kim <kok617@naver.com>
2023-02-06 23:29:18 +0900

    Add: sayHello

--- class.dart
+++ class.dart
@@ -2,15 +2,16 @@ class Player {
   // 클래스 프로퍼티는 타입을 지정.
   String name = 'kim';
   int xp = 1500;
+
+  void sayHello() {
+    // dart 는 this 미권장.
+    print('Hi my name is $name');
+  }
 }
 
 void main() {
   // 메소드 프로퍼티는 var 를 사용.
   var player = Player();
 
-  print(player.name);
-
-  player.name = 'lee';
-
-  print(player.name);
+  player.sayHello();
 }
