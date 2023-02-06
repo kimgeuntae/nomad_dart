c643b89c1e04de584fd149cfc250eb9fd654980b
geuntae.kim <kok617@naver.com>
2023-02-06 23:42:36 +0900

    Add: Player constuctor

--- class.dart
+++ class.dart
@@ -1,7 +1,13 @@
 class Player {
   // 클래스 프로퍼티는 타입을 지정.
-  String name = 'kim';
-  int xp = 1500;
+  late String name;
+  late int xp;
+
+// Constructors
+  Player(String name, int xp) {
+    this.name = name;
+    this.xp = xp;
+  }
 
   void sayHello() {
     // dart 는 this 미권장.
@@ -11,7 +17,7 @@ class Player {
 
 void main() {
   // 메소드 프로퍼티는 var 를 사용.
-  var player = Player();
+  var player = Player('kim', 150);
 
   player.sayHello();
 }
