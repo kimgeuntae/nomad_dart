99b1b10d4670388e578710304b09169743b1a05d
geuntae.kim <kok617@naver.com>
2023-02-06 23:20:28 +0900

    Add: Player

--- class.dart
+++ class.dart
@@ -1,1 +1,16 @@
-void main() {}
+class Player {
+  // 클래스 프로퍼티는 타입을 지정.
+  String name = 'kim';
+  int xp = 1500;
+}
+
+void main() {
+  // 메소드 프로퍼티는 var 를 사용.
+  var player = Player();
+
+  print(player.name);
+
+  player.name = 'lee';
+
+  print(player.name);
+}
